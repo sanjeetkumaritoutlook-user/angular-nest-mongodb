c2243e29154817ef7cbcc4aa6f1191d2c58c6625
sanjeetkumaritoutlook-user <sanjeetkumarit@outlook.com>
2025-10-19 17:16:26 +0530

    integrate frontend to nestjs deployed on Render(downtime sometimes)

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/items'
+  // apiUrl: 'http://localhost:3000/items'
+   apiUrl: 'https://angular-nest-mongodb.onrender.com/items'
 };
